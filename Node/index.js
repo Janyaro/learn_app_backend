7e6c78e9e015ce60eaf7fa32099dd54308187c54
wasim akram janyaro <waseemjanyaro@gmail.com>
2025-05-21 21:14:01 +0500

    dsda

---
+++ Node/index.js
@@ -0,0 +1,2 @@
+const math = require('./user.module');
+console.log(`the sum of the two number is ${math.subtractTwoNumber(2,5)}`);
---
+++ Node/user.module.js
@@ -0,0 +1,14 @@
+// user.module.js
+ const sumTwoNumber = (x, y) => {
+  return x+y;
+};
+
+
+ const subtractTwoNumber = (x, y) => {
+  return x-y;
+};
+
+module.exports={
+    sumTwoNumber,
+    subtractTwoNumber
+}
